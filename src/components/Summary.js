1e6ddd9fa8de5c58bc3ba7ee374764838126c86f
sherwino <s.garcia@applicaster.com>
2020-10-22 01:37:35 -0400

    fix: remove ipsum from summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,30 +1,32 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 
+// TODO: pull this data from CMS and markdown
 export const Summary = () => {
   return (
     <section id="two">
-    <div className="inner">
-      <header className="major">
-        <h2>Massa libero</h2>
-      </header>
-      <p>
-        Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-        libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-        Pellentesque condimentum sem. In efficitur ligula tate urna.
-        Maecenas laoreet massa vel lacinia pellentesque lorem ipsum
-        dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et
-        sagittis libero. Mauris aliquet magna magna sed nunc rhoncus
-        amet pharetra et feugiat tempus.
-      </p>
-      <ul className="actions">
-        <li>
-          <a href="landing.html" className="button next">
-            Get Started
-          </a>
-        </li>
-      </ul>
-    </div>
-    </section>    
+      <div className="inner">
+        <header className="major">
+          <h2>What we do</h2>
+        </header>
+        <p>
+          If you need somebody, an engineering technician who makes detailed
+          technical drawings or plans for machinery, buildings, electronics,
+          infrastructure, and etc don't hesitate to react out. We use software
+          such as AutoCAD, SolidWorks and other 3D modeling tools to convert the
+          designs, plans, and layouts of engineers and architects into a set of
+          technical drawings. JER Engineering and Design, can also generate and
+          create all of the documents you need to fabricate and bring your ideas
+          to life.
+        </p>
+        <ul className="actions">
+          <li>
+            <Link to="/about" className="button next">
+              Learn more about JER.
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </section>
   )
 }
